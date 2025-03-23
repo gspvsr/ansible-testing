179bafb445ef9a81a8f4153bb7281a171eac0154
gspvsr <gspvsr@yahoo.com>
2025-03-23 17:08:20 -0500

    changes done

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -4,7 +4,7 @@ data "aws_vpc" "ansible_vpc" {
 
 data "aws_route_table" "ansible_vpc_rt" {
   subnet_id = "subnet-0150cb12f5b6c2744"
-  # # If subnet_id giving errors use route table id as below
+  # If subnet_id gives errors, use route_table_id as below:
   # route_table_id = var.route_table_id
 }
 
@@ -12,6 +12,7 @@ resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
   peer_vpc_id = data.aws_vpc.ansible_vpc.id
   vpc_id      = aws_vpc.default.id
   auto_accept = true
+
   accepter {
     allow_remote_vpc_dns_resolution = true
   }
@@ -29,12 +30,10 @@ resource "aws_route" "peering-to-ansible-vpc" {
   route_table_id            = aws_route_table.terraform-public.id
   destination_cidr_block    = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
-  #depends_on                = [aws_route_table.terraform-public]
 }
 
 resource "aws_route" "peering-from-ansible-vpc" {
   route_table_id            = data.aws_route_table.ansible_vpc_rt.id
   destination_cidr_block    = "10.37.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
-  #depends_on                = [aws_route_table.terraform-public]
 }
