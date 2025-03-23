49a14163b19a446483b4fc1f9ae6b93aa4d8c973
gspvsr <gspvsr@yahoo.com>
2025-03-23 16:59:24 -0500

    changes done

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -3,9 +3,9 @@ data "aws_vpc" "ansible_vpc" {
 }
 
 data "aws_route_table" "ansible_vpc_rt" {
-  subnet_id = "subnet-0150cb12f5b6c2744"
-  # If subnet_id giving errors use route table id as below
-  # route_table_id = data.aws_route_table.ansible_vpc_rt.id
+  # subnet_id = "subnet-0150cb12f5b6c2744"
+  # # If subnet_id giving errors use route table id as below
+  route_table_id = data.aws_route_table.ansible_vpc_rt.id
 }
 
 resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
